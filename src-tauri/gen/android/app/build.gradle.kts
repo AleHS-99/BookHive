f5db78866c461252a23fbe3fa6a9796f840b514e
AleHS-99 <ahsilva745@gmail.com>
2026-08-17 14:17:40 -0400

    com.bookhive.app

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -19,10 +19,10 @@ val tauriProperties = Properties().apply {
 
 android {
     compileSdk = 36
-    namespace = "com.alehs.bookhive"
+    namespace = "com.bookhive.app"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
-        applicationId = "com.alehs.bookhive"
+        applicationId = "com.bookhive.app"
         minSdk = 24
         targetSdk = 36
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
